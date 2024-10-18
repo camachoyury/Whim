c6a5a9bd1ac5c783b534f7b38b4eae7a5c0c68ee
yury.camacho <yury.camacho@rappi.com>
2024-10-18 09:55:22 -0400

    Initial commit

---
+++ app/src/main/java/com/camachoyury/whim/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.camachoyury.whim
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.camachoyury.whim.ui.theme.WhimTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      WhimTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background,
+        ) {
+          BakingScreen()
+        }
+      }
+    }
+  }
+}
