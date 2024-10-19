d84255812d954666b7836bc19d769467bd81f2a8
yury.camacho <yury.camacho@rappi.com>
2024-10-19 01:39:33 -0500

    image from camera

--- app/src/main/java/com/camachoyury/whim/BakingScreen.kt
+++ app/src/main/java/com/camachoyury/whim/BakingScreen.kt
@@ -1,6 +1,15 @@
 package com.camachoyury.whim
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -11,6 +20,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
@@ -30,121 +40,218 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 val images = arrayOf(
-  // Image generated using Gemini from the prompt "cupcake image"
-  R.drawable.baked_goods_1,
-  // Image generated using Gemini from the prompt "cookies images"
-  R.drawable.baked_goods_2,
-  // Image generated using Gemini from the prompt "cake images"
-  R.drawable.baked_goods_3,
+    // Image generated using Gemini from the prompt "cupcake image"
+    R.drawable.baked_goods_1,
+    // Image generated using Gemini from the prompt "cookies images"
+    R.drawable.baked_goods_2,
+    // Image generated using Gemini from the prompt "cake images"
+    R.drawable.baked_goods_3
 )
 val imageDescriptions = arrayOf(
-  R.string.image1_description,
-  R.string.image2_description,
-  R.string.image3_description,
+    R.string.image1_description,
+    R.string.image2_description,
+    R.string.image3_description
 )
 
+
+@RequiresApi(Build.VERSION_CODES.P)
 @Composable
 fun BakingScreen(
-  bakingViewModel: BakingViewModel = viewModel()
+    bakingViewModel: BakingViewModel = viewModel()
 ) {
-  val selectedImage = remember { mutableIntStateOf(0) }
-  val placeholderPrompt = stringResource(R.string.prompt_placeholder)
-  val placeholderResult = stringResource(R.string.results_placeholder)
-  var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
-  var result by rememberSaveable { mutableStateOf(placeholderResult) }
-  val uiState by bakingViewModel.uiState.collectAsState()
-  val context = LocalContext.current
+    val selectedImage = remember { mutableIntStateOf(0) }
+    val placeholderPrompt = stringResource(R.string.prompt_placeholder)
+    val placeholderResult = stringResource(R.string.results_placeholder)
+    var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
+    var result by rememberSaveable { mutableStateOf(placeholderResult) }
+    val uiState by bakingViewModel.uiState.collectAsState()
+    val context = LocalContext.current
 
-  Column(
-    modifier = Modifier.fillMaxSize()
-  ) {
-    Text(
-      text = stringResource(R.string.baking_title),
-      style = MaterialTheme.typography.titleLarge,
-      modifier = Modifier.padding(16.dp)
+    val bitmap =
+        remember { mutableStateOf(BitmapFactory.decodeResource(context.resources, R.drawable.place_holder_photo)) }
+
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
     )
 
-    LazyRow(
-      modifier = Modifier.fillMaxWidth()
-    ) {
-      itemsIndexed(images) { index, image ->
-        var imageModifier = Modifier
-          .padding(start = 8.dp, end = 8.dp)
-          .requiredSize(200.dp)
-          .clickable {
-            selectedImage.intValue = index
-          }
-        if (index == selectedImage.intValue) {
-          imageModifier =
-            imageModifier.border(BorderStroke(4.dp, MaterialTheme.colorScheme.primary))
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+    // Launcer de la camara
+    val cameraLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.TakePicture()) {
+            capturedImageUri = uri
         }
-        Image(
-          painter = painterResource(image),
-          contentDescription = stringResource(imageDescriptions[index]),
-          modifier = imageModifier
-        )
-      }
-    }
 
-    Row(
-      modifier = Modifier.padding(all = 16.dp)
+    //Permisos
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
     ) {
-      TextField(
-        value = prompt,
-        label = { Text(stringResource(R.string.label_prompt)) },
-        onValueChange = { prompt = it },
-        modifier = Modifier
-          .weight(0.8f)
-          .padding(end = 16.dp)
-          .align(Alignment.CenterVertically)
-      )
-
-      Button(
-        onClick = {
-          val bitmap = BitmapFactory.decodeResource(
-            context.resources,
-            images[selectedImage.intValue]
-          )
-          bakingViewModel.sendPrompt(bitmap, prompt)
-        },
-        enabled = prompt.isNotEmpty(),
-        modifier = Modifier
-          .align(Alignment.CenterVertically)
-      ) {
-        Text(text = stringResource(R.string.action_go))
-      }
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    if (uiState is UiState.Loading) {
-      CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-    } else {
-      var textColor = MaterialTheme.colorScheme.onSurface
-      if (uiState is UiState.Error) {
-        textColor = MaterialTheme.colorScheme.error
-        result = (uiState as UiState.Error).errorMessage
-      } else if (uiState is UiState.Success) {
-        textColor = MaterialTheme.colorScheme.onSurface
-        result = (uiState as UiState.Success).outputText
-      }
-      val scrollState = rememberScrollState()
-      Text(
-        text = result,
-        textAlign = TextAlign.Start,
-        color = textColor,
-        modifier = Modifier
-          .align(Alignment.CenterHorizontally)
-          .padding(16.dp)
-          .fillMaxSize()
-          .verticalScroll(scrollState)
-      )
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.baking_title),
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+            LazyRow(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                itemsIndexed(images) { index, image ->
+                    var imageModifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .requiredSize(200.dp)
+                        .clickable {
+                            selectedImage.intValue = index
+                        }
+
+                    if (index == selectedImage.intValue) {
+                        imageModifier =
+                            imageModifier.border(
+                                BorderStroke(
+                                    4.dp,
+                                    MaterialTheme.colorScheme.primary
+                                )
+                            )
+                    }
+                    Image(
+                        painter = painterResource(image),
+                        contentDescription = stringResource(imageDescriptions[index]),
+                        modifier = imageModifier
+                    )
+                }
+
+                item {
+                    Image(
+                        bitmap = bitmap.value.asImageBitmap(),
+                        contentDescription = "",
+
+                        modifier = Modifier
+                            .padding(start = 8.dp, end = 8.dp)
+                            .requiredSize(200.dp)
+                            .clickable {
+                                    println("TODO: Add image picker")
+                                    val permissionCheckResult =
+                                        ContextCompat.checkSelfPermission(
+                                            context,
+                                            android.Manifest.permission.CAMERA
+                                        )
+                                    if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                                        cameraLauncher.launch(uri)
+
+                                    } else {
+                                        // Request a permission
+                                        permissionLauncher.launch(android.Manifest.permission.CAMERA)
+                                    }
+                            }
+                    )
+                }
+                if (capturedImageUri.path?.isNotEmpty() == true) {
+                    val cameraImage = ImageDecoder.decodeBitmap(
+                        ImageDecoder.createSource(
+                            context.contentResolver,
+                            capturedImageUri
+                        )
+                    )
+                    bitmap.value = cameraImage
+                }
+            }
+
+        Row(
+            modifier = Modifier.padding(all = 16.dp)
+        ) {
+            TextField(
+                value = prompt,
+                label = { Text(stringResource(R.string.label_prompt)) },
+                onValueChange = { prompt = it },
+                modifier = Modifier
+                    .weight(0.8f)
+                    .padding(end = 16.dp)
+                    .align(Alignment.CenterVertically)
+            )
+
+            Button(
+                onClick = {
+                    if (selectedImage.intValue != images.lastIndex) {
+
+                        bitmap.value = BitmapFactory.decodeResource(
+                            context.resources,
+                            images[selectedImage.intValue]
+                        )
+                    }
+
+                    bakingViewModel.sendPrompt(bitmap.value, prompt)
+                },
+                enabled = prompt.isNotEmpty(),
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = stringResource(R.string.action_go))
+            }
+        }
+
+        if (uiState is UiState.Loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+        } else {
+            var textColor = MaterialTheme.colorScheme.onSurface
+            if (uiState is UiState.Error) {
+                textColor = MaterialTheme.colorScheme.error
+                result = (uiState as UiState.Error).errorMessage
+            } else if (uiState is UiState.Success) {
+                textColor = MaterialTheme.colorScheme.onSurface
+                result = (uiState as UiState.Success).outputText
+            }
+            val scrollState = rememberScrollState()
+            Text(
+                text = result,
+                textAlign = TextAlign.Start,
+                color = textColor,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+            )
+        }
     }
-  }
+}
+
+fun Context.createImageFile(): File {
+    // Create an image file name
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+    val image = File.createTempFile(
+        imageFileName, /* prefix */
+        ".jpg", /* suffix */
+        externalCacheDir      /* directory */
+    )
+    return image
 }
